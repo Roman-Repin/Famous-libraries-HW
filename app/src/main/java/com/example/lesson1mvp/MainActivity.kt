9ef969eeb47ded13aafdab4e6c3dd09407fe547b
Repin_Roman <repinroman123@yandex.ru>
2023-01-22 23:55:39 +0300

    HW.Lesson1. Убрал из presenter части android frameworks. Подредачил модель.

--- app/src/main/java/com/example/lesson1mvp/MainActivity.kt
+++ app/src/main/java/com/example/lesson1mvp/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.lesson1mvp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.lesson1mvp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainView {
+
+    private val presenter = MainPresenter(this)
+
+    private var _vb: ActivityMainBinding? = null
+
+    private val vb
+        get() = _vb!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _vb = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(vb.root)
+        presenter.initButton()
+
+        vb.btnCounter1.setOnClickListener { presenter.firstCounterClick() }
+        vb.btnCounter2.setOnClickListener { presenter.secondCounterClick() }
+        vb.btnCounter3.setOnClickListener { presenter.thirdCounterClick() }
+    }
+
+    override fun setFirstButtonText(text: String) {
+        vb.btnCounter1.text = text
+    }
+
+    override fun setSecondButtonText(text: String) {
+        vb.btnCounter2.text = text
+    }
+
+    override fun setThirdButtonText(text: String) {
+        vb.btnCounter3.text = text
     }
 }
